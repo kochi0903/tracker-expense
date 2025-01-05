f9d623d2ac69cfbf5a4c74787f004f59fd8d22e4
kochi0903 <kroy8900391997@gmail.com>
2025-01-05 20:18:02 +0530

    path fixes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { RouterProvider } from "react-router-dom";
 // Library
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { router } from "./route";
+import { router } from "./Route";
 
 
 function App() {
